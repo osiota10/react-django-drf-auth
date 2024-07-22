0c1616a9b30ff65f5ab8f1989f3ab745dfb80071
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2024-07-22 18:19:19 +0100

    Project config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // Entry point
-
 //npm install --save redux @redux-devtools/extension react-redux redux-thunk @reduxjs/toolkit
+
+export * from './actions/authActions';
+export * from './reducers/authReducer';
