574b196380b9fb874e5700af1d5cbd9420536ee2
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2024-07-22 18:07:03 +0100

    Code cleanup

--- src/store.js
+++ src/store.js
@@ -1,27 +1,8 @@
-// import { createStore, applyMiddleware, compose } from 'redux';
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
 
 const initialState = {};
 
-// const middleware = [thunk];
-
-// const store = configureStore({
-//     reducer: rootReducer,
-//     initialState,
-//     middleware: (getDefaultMiddleware) => enhancedCompose(
-//         enhancedApplyMiddleware(...middleware)
-//     ),
-//     // devTools: process.env.NODE_ENV !== 'production', // Only use dev tools in development
-// });
-
-// const store = configureStore(
-//     rootReducer,
-//     initialState,
-//     composeWithDevTools(applyMiddleware(...middleware))
-// );
 
 const store = configureStore({
     reducer: rootReducer,
