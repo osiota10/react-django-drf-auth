05e0a8c35c56d01c045f3dbc7dee6015f020bc70
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2024-07-23 19:36:57 +0100

    Routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Entry point
 //npm install --save redux @redux-devtools/extension react-redux redux-thunk @reduxjs/toolkit
-
+export { default as AuthProvider } from './App'
 export * from './actions/authActions';
 export * from './reducers/authReducer';
